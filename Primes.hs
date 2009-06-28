eefc3082594a2f09b4881b04e7a85b1c3a0bf2aa
davidb <haskell@davidb.org>
2009-06-28 15:07:52 -0700

    Move Primes into it's own module.

---
+++ Primes.hs
@@ -0,0 +1,35 @@
+----------------------------------------------------------------------
+-- Various utilities associated with prime numbers.
+
+module Primes where
+
+import Data.List (group)
+
+-- Not necessarily in order.
+divisors :: Int -> [Int]
+divisors = spread . primeFactors
+
+spread :: [(Int, Int)] -> [Int]
+spread [] = [1]
+spread ((p,count):r) = 
+   [ res * p^x |
+      res <- spread r,
+      x <- [0..count] ]
+
+primeFactors :: Int -> [(Int, Int)]
+primeFactors n = map (\x -> (head x, length x)) (group answer)
+   where
+      answer = seek n primes
+      seek 1 _ = []
+      seek x fs@(f:fr)
+         | x `mod` f == 0   = f : seek (x `div` f) fs
+         | f > x            = error "Factoring coding error"
+         | otherwise        = seek x fr
+
+primes :: [Int]
+primes = 2 : 3 : [x | x <- [5..], isPrime x]
+
+isPrime :: Int -> Bool
+isPrime n = filter (\x -> n `mod` x == 0) factors == []
+   where
+      factors = takeWhile (<= (floor $ sqrt $ fromIntegral n)) primes
--- pr21.hs
+++ pr21.hs
@@ -13,7 +13,7 @@
 
 module Main where
 
-import Data.List (group)
+import Primes
 
 main :: IO ()
 main = print $ sum $ filter isAmicable [2..10000]
@@ -24,32 +24,3 @@ isAmicable x = pair /= x && pairOf pair == x
 
 pairOf :: Int -> Int
 pairOf x = sum . filter (< x) . divisors $ x
-
--- Not necessarily in order.
-divisors :: Int -> [Int]
-divisors = spread . primeFactors
-
-spread :: [(Int, Int)] -> [Int]
-spread [] = [1]
-spread ((p,count):r) = 
-   [ res * p^x |
-      res <- spread r,
-      x <- [0..count] ]
-
-primeFactors :: Int -> [(Int, Int)]
-primeFactors n = map (\x -> (head x, length x)) (group answer)
-   where
-      answer = seek n primes
-      seek 1 _ = []
-      seek x fs@(f:fr)
-         | x `mod` f == 0   = f : seek (x `div` f) fs
-         | f > x            = error "Factoring coding error"
-         | otherwise        = seek x fr
-
-primes :: [Int]
-primes = 2 : 3 : [x | x <- [5..], isPrime x]
-
-isPrime :: Int -> Bool
-isPrime n = filter (\x -> n `mod` x == 0) factors == []
-   where
-      factors = takeWhile (<= (floor $ sqrt $ fromIntegral n)) primes
