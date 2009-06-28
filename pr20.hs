3f8507abcb2e74335f91363dfd20d6c6530c37b0
David Brown <davidb@davidb.org>
2009-06-27 23:08:03 -0800

    Add problem 20

---
+++ pr20.hs
@@ -0,0 +1,13 @@
+----------------------------------------------------------------------
+-- Find the sum of the digits in the number 100!
+----------------------------------------------------------------------
+
+module Main where
+
+import Data.Char (digitToInt)
+
+main :: IO ()
+main = print answer
+
+answer :: Int
+answer = sum . map digitToInt . show . product $ [2..100]
