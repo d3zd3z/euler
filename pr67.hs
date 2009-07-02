e30876eb4792f0e8263ac50f37cdcee3491c5bbc
David Brown <davidb@davidb.org>
2009-07-02 15:53:39 -0800

    Problem 67

---
+++ pr67.hs
@@ -0,0 +1,52 @@
+----------------------------------------------------------------------
+-- By starting at the top of the triangle below and moving to adjacent
+-- numbers on the row below, the maximum total from top to bottom is
+-- 23.
+--
+--    3
+--   7 5
+--  2 4 6
+-- 8 5 9 3
+--
+-- That is, 3 + 7 + 4 + 9 = 23.
+--
+-- Find the maximum total from top to bottom in triangle.txt (right
+-- click and 'Save Link/Target As...'), a 15K text file containing a
+-- triangle with one-hundred rows.
+--
+-- NOTE: This is a much more difficult version of Problem 18. It is
+-- not possible to try every route to solve this problem, as there are
+-- 2^(99) altogether! If you could check one trillion (10^(12)) routes
+-- every second it would take over twenty billion years to check them
+-- all. There is an efficient algorithm to solve it. ;o)
+----------------------------------------------------------------------
+
+module Main where
+
+main :: IO ()
+main = do
+   t <- readTriangle "triangle.txt"
+   print $ head $ reduce t
+
+simple :: [[Int]]
+simple = reverse [
+   [3],
+   [7,5],
+   [2,4,6],
+   [8,5,9,3]]
+
+readTriangle :: FilePath -> IO [[Int]]
+readTriangle name = do
+   triangle <- readFile name
+   return $ reverse $ map (map read . words) $ lines $ triangle
+
+reduce :: [[Int]] -> [Int]
+reduce [] = error "Nothing to reduce"
+reduce [a] = a
+reduce (a:b:rs) = reduce ((select a b) : rs)
+
+select :: [Int] -> [Int] -> [Int]
+select _ [] = []
+select (a1:a2:as) (b:bs) =
+   (a1 `max` a2) + b : select (a2:as) bs
+select _ _ = error "List got shorter"
