3e3a6ad684a248c59b3e462863c740e4814d0a27
David Brown <davidb@davidb.org>
2009-07-05 20:38:15 -0800

    Problem 55

---
+++ pr55.hs
@@ -0,0 +1,63 @@
+----------------------------------------------------------------------
+-- If we take 47, reverse and add, 47 + 74 = 121, which is
+-- palindromic.
+--
+-- Not all numbers produce palindromes so quickly. For example,
+--
+-- 349 + 943 = 1292,
+-- 1292 + 2921 = 4213
+-- 4213 + 3124 = 7337
+--
+-- That is, 349 took three iterations to arrive at a palindrome.
+--
+-- Although no one has proved it yet, it is thought that some numbers,
+-- like 196, never produce a palindrome. A number that never forms a
+-- palindrome through the reverse and add process is called a Lychrel
+-- number. Due to the theoretical nature of these numbers, and for the
+-- purpose of this problem, we shall assume that a number is Lychrel
+-- until proven otherwise. In addition you are given that for every
+-- number below ten-thousand, it will either (i) become a palindrome
+-- in less than fifty iterations, or, (ii) no one, with all the
+-- computing power that exists, has managed so far to map it to a
+-- palindrome. In fact, 10677 is the first number to be shown to
+-- require over fifty iterations before producing a palindrome:
+-- 4668731596684224866951378664 (53 iterations, 28-digits).
+--
+-- Surprisingly, there are palindromic numbers that are themselves
+-- Lychrel numbers; the first example is 4994.
+--
+-- How many Lychrel numbers are there below ten-thousand?
+--
+-- NOTE: Wording was modified slightly on 24 April 2007 to emphasise
+-- the theoretical nature of Lychrel numbers.
+----------------------------------------------------------------------
+
+module Main where
+
+flipNum :: Integer -> Integer
+flipNum = undigits . digits
+
+main :: IO ()
+main = print $ length $ filter (== 51) $ map (length . take 51 . chain) [1..9999]
+
+-- Returns the flipChain, as long as the numbers are not palindromic.
+chain :: Integer -> [Integer]
+chain = takeWhile (\x -> x /= flipNum x) . flipChain
+
+-- Note that this does not include the original number.
+flipChain :: Integer -> [Integer]
+flipChain n = next : flipChain next
+   where next = n + flipNum n
+
+undigits :: [Int] -> Integer
+-- undigits [] = 0
+-- undigits (n:r) = fromIntegral n + (10 * undigits r)
+undigits = un 0
+   where
+      un x [] = x
+      un x (n:r) = un (x * 10 + fromIntegral n) r
+
+-- Compute digits of a number, but in reverse.
+digits :: Integer -> [Int]
+digits 0 = []
+digits n = fromIntegral (n `mod` 10) : digits (n `div` 10)
