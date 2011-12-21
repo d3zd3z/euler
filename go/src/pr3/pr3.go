c4d45dbc12e6edd13855abd5f8ce5470a7aa523f
David Brown <haskell@davidb.org>
2011-12-20 19:20:30 -0800

    1-7,80 in Go

---
+++ go/src/pr3/pr3.go
@@ -0,0 +1,76 @@
+//////////////////////////////////////////////////////////////////////
+// Problem 3
+// 
+// 02 November 2001
+// 
+// The prime factors of 13195 are 5, 7, 13 and 29.
+// 
+// What is the largest prime factor of the number 600851475143 ?
+// 
+//////////////////////////////////////////////////////////////////////
+
+// The input number fits in an Int64.
+
+package main
+
+import "fmt"
+import "euler"
+
+const start int64 = 600851475143
+
+func main() {
+	var sieve euler.SieveHeap
+
+	num := start
+	for {
+		prime := sieve.Next()
+		if num == prime {
+			fmt.Printf("%d\n", prime)
+			break
+		}
+
+		// Divide out the prime as many times as possible.
+		for num%prime == 0 {
+			num /= prime
+		}
+	}
+
+	// for i := 0; i < 100; i++ {
+	// 	fmt.Printf("%8d\n", sieve.Next())
+	// }
+	bench()
+}
+
+func bench() {
+	var sieve euler.SieveHeap
+
+	stop := int64(10000000)
+	for {
+		prime := sieve.Next()
+		if prime >= stop {
+			// fmt.Printf("%v\n", sieve)
+			return
+		}
+	}
+}
+
+// Integer square root of a "big" number.
+// Turns out to not be needed.
+func isqrt(num int64) (result int64) {
+	bit := int64(1)
+	for (bit << 2) < num {
+		bit <<= 2
+	}
+
+	for bit != 0 {
+		if num >= result+bit {
+			num -= result + bit
+			result = (result >> 1) + bit
+		} else {
+			result >>= 1
+		}
+		bit >>= 2
+	}
+	return
+}
+
---
+++ go/src/pr5/pr5.go
@@ -0,0 +1,39 @@
+//////////////////////////////////////////////////////////////////////
+// Problem 5
+// 
+// 30 November 2001
+// 
+// 2520 is the smallest number that can be divided by each of the
+// numbers from 1 to 10 without any remainder.
+// 
+// What is the smallest positive number that is evenly divisible by all
+// of the numbers from 1 to 20?
+// 
+//////////////////////////////////////////////////////////////////////
+
+package main
+
+import "fmt"
+
+func main() {
+	answer := 1
+	for i := 2; i <= 20; i++ {
+		answer = lcm(answer, i)
+	}
+	fmt.Printf("%d\n", answer)
+}
+
+func lcm(a, b int) int {
+	return (a / gcd(a, b)) * b
+}
+
+// Assumes unsigned.
+func gcd(a, b int) int {
+	for {
+		if b == 0 {
+			return a
+		}
+		a, b = b, a % b
+	}
+	panic("Unreached")
+}
