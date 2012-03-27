c41e11bc1c7fce8e2749cdda0f836ac19b4778e7
David Brown <davidb@davidb.org>
2012-03-26 21:17:53 -0800

    A few new ocaml solutions

---
+++ ocaml/bench.ml
@@ -0,0 +1,16 @@
+(* Simple benchmarks. *)
+
+let repeat thunk n =
+  let start = Unix.gettimeofday () in
+  for i = 1 to n do
+    thunk ()
+  done;
+  (Unix.gettimeofday ()) -. start
+
+let bench thunk =
+  let rec loop count =
+    let time = repeat thunk count in
+    if time < 1.0 then loop (count*2)
+    else (float_of_int count) /. time in
+  let time = loop 1 in
+  Printf.printf "%.3f ops/sec\n%!" time
