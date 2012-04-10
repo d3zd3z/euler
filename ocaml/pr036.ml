501327047c16c05192185b327e62b8e3fb2391c1
David Brown <davidb@davidb.org>
2012-04-09 20:16:04 -0800

    Four more problems

---
+++ ocaml/pr036.ml
@@ -0,0 +1,30 @@
+(*
+ * Problem 36
+ *
+ * 31 January 2003
+ *
+ *
+ * The decimal number, 585 = 1001001001[2] (binary), is palindromic in
+ * both bases.
+ *
+ * Find the sum of all numbers, less than one million, which are
+ * palindromic in base 10 and base 2.
+ *
+ * (Please note that the palindromic number, in either base, may not
+ * include leading zeros.)
+ *)
+
+open Batteries_uni
+open Printf
+
+let is_palindrome ?(base=10) number =
+  number = Euler.reverse_number ~base:base number
+
+let euler36 () =
+  Enum.fold (fun sum number ->
+    if is_palindrome ~base:10 number &&
+      is_palindrome ~base:2 number
+    then sum+number
+    else sum) 0 (1 --^ 1_000_000)
+
+let () = printf "%d\n" (euler36 ())
