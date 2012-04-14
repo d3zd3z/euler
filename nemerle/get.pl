106220be72abd563a88b0038a1208f732826390c
David Brown <davidb@davidb.org>
2012-04-13 23:48:54 -0800

    nemerle work

---
+++ nemerle/get.pl
@@ -0,0 +1,43 @@
+#! /usr/bin/perl
+#
+# You have entered a maze of twisty passages, all alike.
+
+use 5.008;
+use strict;
+
+die "Expecting an argument" unless $#ARGV == 0;
+die "Expecting a single integer" unless $ARGV[0] =~ /([1-9][0-9]*)/;
+
+my $problem = int($1);
+my $prob3 = sprintf("%03d", $problem);
+# my $URL = "http://projecteuler.net/problem=$problem";
+my $URL = sprintf("../haskell/probs/problem-%03d.html", $problem);
+open(I, "w3m -dump -cols 75 $URL|") or die;
+
+open(O, ">pr$prob3.n") or die;
+
+# Skip until we get the problem statement.
+while (<I>) {
+	last if /^Problem /;
+}
+print O "// $_";
+my $last = $_;
+while (<I>) {
+	last if /Project Euler Copyright/;
+	if ($_ ne $last || !/^\s*\n/) {
+		print O  "// $_";
+	}
+	$last = $_;
+}
+print O <<EOF;
+
+using Nemerle.IO;
+
+module Pr$prob3 {
+
+    public Main () : void {
+    }
+}
+EOF
+close I or die "Problem getting euler page";
+close O or die;
