24c2cad141beb63350ef8f01ec3d11d3f4481c75
David Brown <davidb@davidb.org>
2012-05-13 09:03:33 -0800

    Ada problem 24

--- ada/euler.ads
+++ ada/euler.ads
@@ -12,4 +12,17 @@ package Euler is
    procedure Print_Result (Value : Natural);
    procedure Print_Result (Value : Long_Natural);
 
+   generic
+      type Element_Type is private;
+      type Element_Array is array (Positive range <>) of Element_Type;
+
+      with function "<" (Left, Right : Element_Type) return Boolean is <>;
+   procedure Next_Permutation (
+      Items : in out Element_Array;
+      Done  : out Boolean);
+   --  Modify Items in place to compute the next permutation.  If the items is
+   --  already the largest lexical permutation, doesn't modify it, and sets
+   --  Done to True.  Otherwise, the Items will contain the next possible
+   --  permutation, and Done will be set to False.
+
 end Euler;
