c0c94f53c94075e1a50ad309be5caa99a36b290c
David Brown <davidb@davidb.org>
2012-06-19 20:52:58 -0800

    Fortran, problems 1-9

---
+++ fortran/pr006.f90
@@ -0,0 +1,35 @@
+! Problem 6
+!
+! 14 December 2001
+!
+! The sum of the squares of the first ten natural numbers is,
+!
+! 1^2 + 2^2 + ... + 10^2 = 385
+!
+! The square of the sum of the first ten natural numbers is,
+!
+! (1 + 2 + ... + 10)^2 = 55^2 = 3025
+!
+! Hence the difference between the sum of the squares of the first ten
+! natural numbers and the square of the sum is 3025 − 385 = 2640.
+!
+! Find the difference between the sum of the squares of the first one
+! hundred natural numbers and the square of the sum.
+! 25164150
+
+program pr006
+
+  implicit none
+  integer :: i, sumsq, sqsum
+
+  sumsq = 0
+  sqsum = 0
+  do i = 1, 100
+    sumsq = sumsq + i**2
+    sqsum = sqsum + i
+  end do
+  sqsum = sqsum**2
+
+  print *, sqsum - sumsq
+
+end program pr006
---
+++ fortran/pr009.f90
@@ -0,0 +1,33 @@
+! Problem 9
+!
+! 25 January 2002
+!
+! A Pythagorean triplet is a set of three natural numbers, a < b < c,
+! for which,
+!
+! a^2 + b^2 = c^2
+!
+! For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+!
+! There exists exactly one Pythagorean triplet for which a + b + c =
+! 1000.
+! Find the product abc.
+! 31875000
+
+! This particular problem is small enough that we can easily just do a brute
+! force search.
+
+program pr009
+
+  implicit none
+
+  integer :: a, b, c
+
+  do a = 1, 999
+    do b = a, 1000 - a
+      c = 1000 - a - b
+      if (a*a + b*b == c*c) print *, (a*b*c)
+    end do
+  end do
+
+end program pr009
