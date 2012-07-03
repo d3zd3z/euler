6bb79c79974b82931af1f0ee732a9bb2d7307e98
David Brown <haskell@davidb.org>
2012-07-02 23:47:21 -0700

    rust: update 10 to rust 0.2

--- rust/pr010.rs
+++ rust/pr010.rs
@@ -5,18 +5,20 @@
 // The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 // 
 // Find the sum of all the primes below two million.
+//
+// 142913828922
 
 use std;
 
 fn main() {
-    let primes = sieve::make(2000200u);
+    let mut primes = sieve::make(2000200u);
 
-    let total = 0u;
-    let p = 2u;
+    let mut total = 0u;
+    let mut p = 2u;
     while p < 2_000_000u {
         total += p;
         p = sieve::next_prime(primes, p);
     }
 
-    std::io::println(#fmt("%u", total));
+    io::println(#fmt("%u", total));
 }
