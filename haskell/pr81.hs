52d14001df25719ad585149d90b563bfa863585b
David Brown <haskell@davidb.org>
2012-08-09 23:21:19 -0700

    Continue work on problem 81

--- haskell/pr81.hs
+++ haskell/pr81.hs
@@ -25,42 +25,89 @@
 
 module Main where
 
-import Data.Functor
+-- import Data.Functor
+import Data.List (transpose, zipWith)
+import Data.Map (Map)
+import qualified Data.Map as Map
+import Data.Tuple (swap)
+import Data.Set (Set)
+import qualified Data.Set as Set
 
-data Node = Node { nodeValue :: Int, nodeNext :: [Node] }
---           deriving Show
--- Not sure how useful Show really is, since it kind of explodes.
+type Node = (Int, Int)
+data Edge = Edge { edgeWeight :: Int, edgeNext :: Node }
+          deriving Show
 
--- Interesting, but not really very useful
-makeGraph :: [[Int]] -> Node
-makeGraph lines = case nodeList of
-  ((n:_):_) -> n
-  _ -> error "No nodes"
-  where nodeList = vconnect $ map listToNode lines
+type EdgeMap = Map Node [Edge]
 
--- Make nodes out of a single line.
-listToNode :: [Int] -> [Node]
-listToNode [] = []
-listToNode (a:as) = node : rest
-  where node = Node { nodeValue = a, nodeNext = next }
-        rest = listToNode as
-        next = case rest of
-          [] -> []
-          (n:_) -> [n]
+buildGraph :: [[Int]] -> EdgeMap
+buildGraph matrix =
+  foldl (Map.unionWith (++)) Map.empty [horiz, vert, entry]
+  where horiz = horizontalEdges matrix
+        vert = verticalEdges matrix
+        entry = Map.singleton (0, 0) [Edge { edgeWeight = 0, edgeNext = (1, 1) }]
 
-vconnect :: [[Node]] -> [[Node]]
-vconnect [] = []
-vconnect [a] = [a]
-vconnect (a:b:bs) = anodes : vconnect (b:bs)
-  where anodes = zipWith connect a b
-        connect a b = a { nodeNext = b : nodeNext a }
+-- Add a single node to the map.
+insertEdge :: Node -> Edge -> EdgeMap -> EdgeMap
+insertEdge node edge = Map.insertWith (++) node [edge]
 
--- Read the matrix in.
-getMatrix :: FilePath -> IO [[Int]]
-getMatrix path = do
-  text <- lines <$> readFile path
-  return $ map ((map read) . split (==',')) text
+-- Given a matrix, generate edges (with weights) for the horizontal
+-- connections.
+makeEdges fixNode matrix =
+  let oneRow rowNum row =
+        let each colNum (a:b:rest) =
+              let node = fixNode (rowNum, colNum) in
+              let edge = Edge { edgeWeight = a, edgeNext = fixNode (rowNum, colNum+1) } in
+              (node, [edge]) : each (colNum+1) (b:rest)
+            each _ _ = [] in
+        each 1 row in
+  Map.fromList $ concat $ zipWith oneRow [1..] matrix
 
+horizontalEdges :: [[Int]] -> EdgeMap
+horizontalEdges = makeEdges id
+
+verticalEdges :: [[Int]] -> EdgeMap
+verticalEdges = makeEdges swap . transpose
+
+-- Find all nodes mentioned by this graph that don't have any exits.
+findExits :: EdgeMap -> Set Node
+findExits edgeMap = Set.difference allNodes edgeNodes
+  where edgeNodes = Set.fromList $ Map.keys edgeMap
+        allNodes = Set.fromList $ map edgeNext $ concat $ Map.elems edgeMap
+
+-- data Node = Node { nodeValue :: Int, nodeNext :: [Node] }
+-- --           deriving Show
+-- -- Not sure how useful Show really is, since it kind of explodes.
+--
+-- -- Interesting, but not really very useful
+-- makeGraph :: [[Int]] -> Node
+-- makeGraph lines = case nodeList of
+--   ((n:_):_) -> n
+--   _ -> error "No nodes"
+--   where nodeList = vconnect $ map listToNode lines
+--
+-- -- Make nodes out of a single line.
+-- listToNode :: [Int] -> [Node]
+-- listToNode [] = []
+-- listToNode (a:as) = node : rest
+--   where node = Node { nodeValue = a, nodeNext = next }
+--         rest = listToNode as
+--         next = case rest of
+--           [] -> []
+--           (n:_) -> [n]
+--
+-- vconnect :: [[Node]] -> [[Node]]
+-- vconnect [] = []
+-- vconnect [a] = [a]
+-- vconnect (a:b:bs) = anodes : vconnect (b:bs)
+--   where anodes = zipWith connect a b
+--         connect a b = a { nodeNext = b : nodeNext a }
+--
+-- -- Read the matrix in.
+-- getMatrix :: FilePath -> IO [[Int]]
+-- getMatrix path = do
+--   text <- lines <$> readFile path
+--   return $ map ((map read) . split (==',')) text
+--
 sample :: [[Int]]
 sample = [
   [131, 673, 234, 103, 18],
@@ -68,11 +115,11 @@ sample = [
   [630, 803, 746, 422, 111],
   [537, 699, 497, 121, 956],
   [805, 732, 524, 37, 331]]
-
-split :: (a -> Bool) -> [a] -> [[a]]
-split _ [] = []
-split t s =
-  let (l, s') = break t s in
-  l : case s' of
-    [] -> []
-    (_:s'') -> split t s''
+--
+-- split :: (a -> Bool) -> [a] -> [[a]]
+-- split _ [] = []
+-- split t s =
+--   let (l, s') = break t s in
+--   l : case s' of
+--     [] -> []
+--     (_:s'') -> split t s''
