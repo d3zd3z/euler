4754fa40db569b0109f778367cc5dcfa8ab1877c
David Brown <davidb@davidb.org>
2012-12-19 21:56:27 -0800

    Move sieve into it's own module

--- pascal/pr007.pas
+++ pascal/pr007.pas
@@ -15,101 +15,19 @@ program pr007;
 
 {$mode objfpc}
 
-uses sysutils;
-
-type
-  sieve = class
-  private
-    primes : array of boolean;
-    procedure fill;
-    procedure newLength(need : longint);
-  public
-    constructor create;
-    function isPrime(n : longint) : boolean;
-    function nextPrime(n : longint) : longint;
-  end;
-
-  constructor sieve.create;
-  begin
-    { I don't know if this reliably initializes them. }
-    SetLength(primes, 1024);
-    fill
-  end;
-
-  procedure sieve.newLength(need : longint);
-  var
-    cur : longint;
-  begin
-    cur := length(primes);
-    while need >= cur do
-      cur := cur * 8;
-    setlength(primes, cur);
-    fill
-  end;
-
-  procedure sieve.fill;
-  var
-    i : longint;
-    pos, n : longint;
-    limit : longint;
-  begin
-    limit := length(primes);
-    for i := 0 to limit-1 do
-      primes[i] := true;
-
-    primes[0] := false;
-    primes[1] := false;
-    pos := 2;
-    while pos < limit do
-      begin
-	if primes[pos] then
-	  begin
-	    n := pos + pos;
-	    while n < limit do
-	      begin
-		primes[n] := false;
-		n := n + pos
-	      end;
-	      if pos = 2 then
-		pos := pos + 1
-	      else
-		pos := pos + 2
-	  end
-	else
-	  pos := pos + 2
-      end;
-  end;
-
-  function sieve.isPrime(n : longint) : boolean;
-  begin
-    if n >= length(primes) then
-      newLength(n);
-
-    isPrime := primes[n]
-  end;
-
-  function sieve.nextPrime(n : longint) : longint;
-  begin
-    if n = 2 then
-      nextPrime := 3
-    else begin
-      nextPrime := n + 2;
-      while not isPrime(nextPrime) do
-	nextPrime := nextPrime + 2
-    end;
-  end;
+uses sieve;
 
 var
-  blort : sieve;
+  sv : SimpleSieve;
   p, count : longint;
 
 begin
-  blort := sieve.create;
+  sv := SimpleSieve.create;
   count := 1;
   p := 2;
   while count < 10001 do
     begin
-      p := blort.nextPrime(p);
+      p := sv.nextPrime(p);
       count := count + 1;
     end;
   writeln(p)
---
+++ pascal/sieve.pas
@@ -0,0 +1,92 @@
+unit sieve;
+
+(* A simple, self-scaling prime sieve *)
+
+{$mode objfpc}
+
+interface
+
+type
+  SimpleSieve = class
+  private
+    primes : packed array of boolean;
+    procedure fill;
+    procedure newLength(need : longint);
+  public
+    constructor create;
+    function isPrime(n : longint) : boolean;
+    function nextPrime(n : longint) : longint;
+  end;
+
+implementation
+
+  constructor SimpleSieve.create;
+  begin
+    { I don't know if this reliably initializes them. }
+    SetLength(primes, 1024);
+    fill
+  end;
+
+  procedure SimpleSieve.newLength(need : longint);
+  var
+    cur : longint;
+  begin
+    cur := length(primes);
+    while need >= cur do
+      cur := cur * 8;
+    setlength(primes, cur);
+    fill
+  end;
+
+  procedure SimpleSieve.fill;
+  var
+    i : longint;
+    pos, n : longint;
+    limit : longint;
+  begin
+    limit := length(primes);
+    for i := 0 to limit-1 do
+      primes[i] := true;
+
+    primes[0] := false;
+    primes[1] := false;
+    pos := 2;
+    while pos < limit do
+      begin
+	if primes[pos] then
+	  begin
+	    n := pos + pos;
+	    while n < limit do
+	      begin
+		primes[n] := false;
+		n := n + pos
+	      end;
+	      if pos = 2 then
+		pos := pos + 1
+	      else
+		pos := pos + 2
+	  end
+	else
+	  pos := pos + 2
+      end;
+  end;
+
+  function SimpleSieve.isPrime(n : longint) : boolean;
+  begin
+    if n >= length(primes) then
+      newLength(n);
+
+    isPrime := primes[n]
+  end;
+
+  function SimpleSieve.nextPrime(n : longint) : longint;
+  begin
+    if n = 2 then
+      nextPrime := 3
+    else begin
+      nextPrime := n + 2;
+      while not isPrime(nextPrime) do
+	nextPrime := nextPrime + 2
+    end;
+  end;
+end.
