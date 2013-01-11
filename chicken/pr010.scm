eb45f5bca3410a09a53d8eb2086b76a8f4e6a8a5
David Brown <davidb@davidb.org>
2013-01-10 22:32:25 -0800

    Chicken 8-13

---
+++ chicken/pr010.scm
@@ -0,0 +1,27 @@
+;;; Problem 10
+;;;
+;;; 08 February 2002
+;;;
+;;; The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+;;;
+;;; Find the sum of all the primes below two million.
+;;;
+;;; 142913828922
+;;;
+;;; Note that the result is larger than a 32-bit number.  The primes
+;;; are not, though.
+
+(require-extension numbers)
+
+(declare (uses sieve))
+(import sieve)
+(define (euler-10)
+  (define s (make-sieve))
+  (let loop ((p 2)
+	     (sum 0))
+    (if (>= p 2000000)
+      sum
+      (loop (next-prime s p) (+ sum p)))))
+
+(display (euler-10))
+(newline)
