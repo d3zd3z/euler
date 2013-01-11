30144a8543a82f99cf0d1bac1f9f139e601b33b7
David Brown <haskell@davidb.org>
2013-01-10 22:39:12 -0800

    Rust 24-32

--- rust/pr024.rs
+++ rust/pr024.rs
@@ -17,11 +17,13 @@
 
 // TODO: With rust 0.5, this produces the wrong result.
 
+mod permute;
+
 fn main() {
     let base: ~[mut u8] = vec::to_mut(do vec::from_fn(10) |i| {i as u8});
     let mut done = false;
-    for (999_999u).times() || {
-        next_permutation(base, &mut done);
+    for 999_999u.times() || {
+        permute::next_permutation(base, &mut done);
         assert !done;
     }
     show(vec::from_mut(base));
@@ -31,48 +33,4 @@ fn show(digits: &[u8]) {
     let mut result = ~"";
     for digits.each() |x| { result += u8::str(*x); }
     io::println(result);
-}
-
-// Advance the items to the next permutation.  Sets 'done' to true if
-// there are no more permutations.
-fn next_permutation(items: &[mut u8], done: &mut bool) {
-    let size = vec::len(items);
-    let mut k = uint::max_value;
-    for uint::range(0, size - 1) |x| {
-        if items[x] < items[x+1] {
-            k = x;
-        }
-    }
-    if k == uint::max_value {
-        *done = true;
-        return
-    }
-
-    let mut l = uint::max_value;
-    for uint::range(k + 1, size) |x| {
-        if items[k] < items[x] {
-            l = x;
-        }
-    }
-
-    swap(items, k, l);
-    flip(items, k + 1, size - 1);
-
-    *done = false;
-}
-
-fn flip(items: &[mut u8], a: uint, b: uint) {
-    let mut aa = a;
-    let mut bb = b;
-    while aa < bb {
-        swap(items, aa, bb);
-        aa += 1;
-        bb -= 1;
-    }
-}
-
-fn swap(items: &[mut u8], a: uint, b: uint) {
-    let tmp = items[a];
-    items[a] = items[b];
-    items[b] = tmp;
 }
