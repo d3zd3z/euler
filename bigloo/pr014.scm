12a7066e790d14884cf19f12e6869e59061a33ac
David Brown <davidb@davidb.org>
2013-01-10 22:31:25 -0800

    Bigloo: cached version of 14

--- bigloo/pr014.scm
+++ bigloo/pr014.scm
@@ -39,7 +39,7 @@
       (loop (chain-next n)
 	    (+ count 1)))))
 
-(define (longest)
+(define (longest chain-len)
   (let loop ((n 1)
 	     (longest-term 1)
 	     (longest-length 0))
@@ -51,9 +51,37 @@
 	  (loop (+ n 1) longest-term longest-length))))))
 
 ;;; TODO: Cached version of this.
+;;; Compute a cached version by keeping an array of learned counts.
+;;; These are initially #f, and we fill them in as we learn them.
+
+(define (make-cache size)
+  (make-vector size #f))
+
+(define (cached-chain-len n cache)
+  (define cache-limit (vector-length cache))
+  (define (go-next n)
+    (let* ((next-num (if (odd? n)
+		       (+ (* n 3) 1)
+		       (quotient n 2)))
+	   (result (+ 1 (cached-chain-len next-num cache))))
+      (when (< n cache-limit)
+	(vector-set! cache n result))
+      result))
+  (define (next n)
+    (if (= n 1) 1
+      (go-next n)))
+
+  (if (< n cache-limit)
+    (let ((v (vector-ref cache n)))
+      (if v v
+	(next n)))
+    (next n)))
 
 (define (euler-14)
-  (longest))
+  (define cache (make-cache 100000))
+  ; (longest chain-len)
+  ; (longest (cut cached-chain-len <> cache))
+  (longest (lambda (n) (cached-chain-len n cache))))
 
 (define (main argv)
   (display (mytime (euler-14)))
