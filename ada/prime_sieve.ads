55cf054f9f11016150724d1622fcc4a7c16e3f52
David Brown <haskell@davidb.org>
2013-02-14 18:50:56 -0800

    Ada: Clean up the prime sieve
    Move the Sieve object into a child package inside of the prime sieve,
    and only allocate it internally.  It needs to be in a package so that
    the object system will work (even though we don't use it, it still
    generates a warning).  Next_Prime can now properly allocate the sieve
    as it needs to.

--- ada/prime_sieve.ads
+++ ada/prime_sieve.ads
@@ -3,20 +3,12 @@
 ----------------------------------------------------------------------
 
 with Ada.Containers.Vectors;
-with Ada.Finalization;
 
 package Prime_Sieve is
 
-   type T (Limit : Natural) is tagged limited private;
-
-   function Is_Prime (Object : T; Index : Natural) return Boolean;
    function Is_Prime (Index : Natural) return Boolean;
 
-   function Next_Prime (Object : T; Index : Natural) return Natural;
    function Next_Prime (Index : Natural) return Natural;
-
-   function Sieve (Limit : Natural) return access T;
-   --  Return a shared sieve capable of handling at least up to Limit.
 
    type Factor is
       record
@@ -45,16 +37,6 @@ package Prime_Sieve is
    --  Return the sum of the proper divisors of the given number.
 
 private
-
-   type Bit_Array is array (Positive range <>) of Boolean;
-   pragma Pack (Bit_Array);
-
-   type T (Limit : Natural) is new Ada.Finalization.Limited_Controlled with
-      record
-         Primes : Bit_Array (1 .. Limit) := (others => True);
-      end record;
-
-   procedure Initialize (Object : in out T);
 
    pragma Inline (Is_Prime);
 
