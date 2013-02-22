b0522157e4244b92699abc74d2184d348d1d0abd
David Brown <davidb@davidb.org>
2013-02-21 21:38:13 -0800

    CL: Cleanup 30-39

--- lisp/pr033.lisp
+++ lisp/pr033.lisp
@@ -1,26 +1,27 @@
 ;;; Problem 33
-;;; 
+;;;
 ;;; 20 December 2002
-;;; 
+;;;
 ;;; The fraction ^49/[98] is a curious fraction, as an inexperienced
 ;;; mathematician in attempting to simplify it may incorrectly believe that ^
 ;;; 49/[98] = ^4/[8], which is correct, is obtained by cancelling the 9s.
-;;; 
+;;;
 ;;; We shall consider fractions like, ^30/[50] = ^3/[5], to be trivial
 ;;; examples.
-;;; 
+;;;
 ;;; There are exactly four non-trivial examples of this type of fraction, less
 ;;; than one in value, and containing two digits in the numerator and
 ;;; denominator.
-;;; 
+;;;
 ;;; If the product of these four fractions is given in its lowest common
 ;;; terms, find the value of the denominator.
-;;; 
+;;;
+;;; 100
 
-(defpackage #:pr33
+(defpackage #:pr033
   (:use #:cl #:iterate)
   (:export #:euler-33))
-(in-package #:pr33)
+(in-package #:pr033)
 
 (defun frac-a-p (a b)
   (multiple-value-bind (an am) (floor a 10)
--- lisp/pr034.lisp
+++ lisp/pr034.lisp
@@ -1,19 +1,20 @@
 ;;; Problem 34
-;;; 
+;;;
 ;;; 03 January 2003
-;;; 
+;;;
 ;;; 145 is a curious number, as 1! + 4! + 5! = 1 + 24 + 120 = 145.
-;;; 
+;;;
 ;;; Find the sum of all numbers which are equal to the sum of the factorial of
 ;;; their digits.
-;;; 
+;;;
 ;;; Note: as 1! = 1 and 2! = 2 are not sums they are not included.
-;;; 
+;;;
+;;; 40730
 
-(defpackage #:pr34
+(defpackage #:pr034
   (:use #:cl #:iterate #:alexandria)
   (:export #:euler-34))
-(in-package #:pr34)
+(in-package #:pr034)
 
 (defun factorials (n)
   "Return a vector of the factorials indexed by n, with n at the top"
