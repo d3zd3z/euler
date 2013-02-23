896586cf94b32bd748a2692831d77683fe884abc
David Brown <haskell@davidb.org>
2013-02-22 19:59:10 -0800

    CL: Cleanup 40-49

--- lisp/pr041.lisp
+++ lisp/pr041.lisp
@@ -1,18 +1,19 @@
 ;;; Problem 41
-;;; 
+;;;
 ;;; 11 April 2003
-;;; 
+;;;
 ;;; We shall say that an n-digit number is pandigital if it makes use of all
 ;;; the digits 1 to n exactly once. For example, 2143 is a 4-digit pandigital
 ;;; and is also prime.
-;;; 
+;;;
 ;;; What is the largest n-digit pandigital prime that exists?
-;;; 
+;;;
+;;; 7652413
 
-(defpackage #:pr41
-  (:use #:cl #:iterate #:pr24 #:mr-prime)
+(defpackage #:pr041
+  (:use #:cl #:iterate #:pr024 #:mr-prime)
   (:export #:euler-41))
-(in-package #:pr41)
+(in-package #:pr041)
 
 (defun euler-41 ()
   (iter (for item first (copy-seq "1234567") then (next-permutation item :less #'char<))
--- lisp/pr043.lisp
+++ lisp/pr043.lisp
@@ -1,14 +1,14 @@
 ;;; Problem 43
-;;; 
+;;;
 ;;; 09 May 2003
-;;; 
+;;;
 ;;; The number, 1406357289, is a 0 to 9 pandigital number because it is made
 ;;; up of each of the digits 0 to 9 in some order, but it also has a rather
 ;;; interesting sub-string divisibility property.
-;;; 
+;;;
 ;;; Let d[1] be the 1^st digit, d[2] be the 2^nd digit, and so on. In this
 ;;; way, we note the following:
-;;; 
+;;;
 ;;;   • d[2]d[3]d[4]=406 is divisible by 2
 ;;;   • d[3]d[4]d[5]=063 is divisible by 3
 ;;;   • d[4]d[5]d[6]=635 is divisible by 5
@@ -16,14 +16,15 @@
 ;;;   • d[6]d[7]d[8]=572 is divisible by 11
 ;;;   • d[7]d[8]d[9]=728 is divisible by 13
 ;;;   • d[8]d[9]d[10]=289 is divisible by 17
-;;; 
+;;;
 ;;; Find the sum of all 0 to 9 pandigital numbers with this property.
-;;; 
+;;;
+;;; 16695334890
 
-(defpackage #:pr43
-  (:use #:cl #:iterate #:pr24)
+(defpackage #:pr043
+  (:use #:cl #:iterate #:pr024)
   (:export #:euler-43))
-(in-package #:pr43)
+(in-package #:pr043)
 
 (defun decode-number (digits &aux (result 0))
   (iter (for i in-vector digits)
