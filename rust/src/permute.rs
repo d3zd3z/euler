96053ebf5ebda2479707c3c7e4babab2f97322db
David Brown <davidb@davidb.org>
2015-01-15 23:36:54 -0800

    rust: Play with permutation iterators
    Rust's libstd contains a permutation iterator that could be useful for
    some of these problems.  Since it clones the result each time, as this
    benchmark code shows, it is quite a bit slower (about 6.5 times for
    me).  Aside from performance, it returns the permutation in a
    single-swap order instead of in lexicographical order.  The swap order
    is easier to compute, but some of the Euler problems need the
    lexicographical.
    
    This change also leaves in several attempts to make an iterator that
    can avoid copying.  None of these can pass the borrow checker.  It
    seems to be an incompatibility between the iterator protocol, and
    something that wants to return the whole container.  Since the
    iterator must mutably hold the data, it isn't possible to (safely) get
    at this same underlying data.
    
    All in all, this code doesn't actually change any of the permutation
    code, since, clumsy as it is to use, it is the most efficient way to
    get all of the permutations.

--- rust/src/permute.rs
+++ rust/src/permute.rs
@@ -2,8 +2,106 @@
 
 use std::uint;
 
-// Advance the items to the next permutation.  Sets 'done' to true if
-// there are no more permutations.
+// TODO: Figure out if there is some way of implementing this iterator.
+
+/*
+// The iterator is a little awkward.  It iterates and mutates the original
+// data, but can't actually return it (without copying it).  So, the iterator
+// just returns (), and the caller should refer to the original data (which has
+// to be around anyway because of the borrow.
+pub struct Iter<'a> {
+    data: &'a mut [u8],
+}
+
+impl<'a> Iter<'a> {
+    pub fn new<'b>(s: &'b mut [u8]) -> Iter<'b> {
+        Iter { data: s }
+    }
+}
+
+impl<'a> Iterator for Iter<'a> {
+    type Item = &'a mut Iter<'a>;
+
+    fn next(&mut self) -> Option<&'a mut Iter<'a>> {
+        let mut done = true;
+        next_permutation(self.data, &mut done);
+        if done { None }
+        else { Some(self) }
+    }
+}
+
+#[test]
+fn test_permute() {
+    let mut work = vec!(1, 2, 3);
+    for () in Iter::new(work.as_mut_slice()) {
+        println!("p: {:?}", work);
+    }
+}
+*/
+
+/*
+ * Another attempt that doesn't work.  This time, it can't come up with
+ * something to return.  This one is probably actually safe, but probably needs
+ * a safety indicator from the borrow checker.
+// Container to hold the mutator itself.
+pub struct Permuter(Vec<u8>);
+
+impl Permuter {
+    pub fn iter<'a>(&'a mut self) -> Iter<'a> {
+        Iter { data: &self.0[] }
+    }
+}
+
+pub struct Iter<'a> {
+    data: &'a mut [u8],
+}
+
+impl<'a> Iterator for Iter<'a> {
+    type Item = &'a [u8];
+
+    fn next(&mut self) -> Option<&'a [u8]> {
+        let mut done;
+        next_permutation(self.data, &mut done);
+        if done {
+            None
+        } else {
+            Some(self.data)
+        }
+    }
+}
+*/
+
+/*
+ * First attempt.  Can't pass borrow checker.
+ * There isn't any way to make this safe with the data actually stored in the
+ * iterator itself.
+pub struct Iter {
+    data: Vec<u8>
+}
+
+impl Iter {
+    pub fn new_vec(data: Vec<u8>) -> Iter {
+        Iter { data: data }
+    }
+}
+
+impl<'a> Iterator for Iter {
+    type Item = &'a [u8];
+
+    fn next(&mut self) -> Option<&'a [u8]> {
+        let mut done;
+        next_permutation(&self.data[], &mut done);
+        if done {
+            None
+        } else {
+            Some(&self.data[])
+        }
+    }
+}
+*/
+
+/// Advance the items to the next permutation.  Sets 'done' to true if
+/// there are no more permutations.
 pub fn next_permutation(items: &mut [u8], done: &mut bool) {
     let size = items.len();
     let mut k = uint::MAX;
@@ -44,4 +142,42 @@ fn swap(items: &mut [u8], a: uint, b: uint) {
     let tmp = items[a];
     items[a] = items[b];
     items[b] = tmp;
+}
+
+/// A basic benchmark of the permutations
+/// libstd contains a `permutations` method, but the iterator clones.  Since
+/// the iteration requires a mutable vector, there doesn't seem any safe way of
+/// doing this with the normal iterator protocol.  As of recent tests, just
+/// using next_permutation instead of the iterator is about 6.5 times faster.
+///
+/// Also, some of the euler problems require the permutation to be done
+/// strictly in lexicographical order, which the `permutation()` method does
+/// not do.
+#[cfg(test)]
+mod bench {
+    use super::*;
+    const BENCH_SIZE: u8 = 6;
+
+    #[bench]
+    fn bench_myperm(b: &mut ::test::Bencher) {
+        b.iter(|| {
+            let mut nums: Vec<u8> = (0 .. BENCH_SIZE).collect();
+            let mut done = false;
+
+            loop {
+                next_permutation(&mut nums[], &mut done);
+                if done { break; }
+            }
+        });
+    }
+
+    // Also benchmark the cloning iterator part of normal slices.
+    #[bench]
+    fn bench_stdperm(b: &mut ::test::Bencher) {
+        b.iter(|| {
+            let nums: Vec<u8> = (0 .. BENCH_SIZE).collect();
+            for _pv in nums.permutations() {
+            }
+        });
+    }
 }
