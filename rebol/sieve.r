0abd99ad0956da0512a70ae94aacc39ec0a5c961
David Brown <davidb@davidb.org>
2015-04-24 11:12:32 -0700

    Rebol: Use prototype object for sieve
    Clean up the definition of the sieve a bit by putting most of the work
    in a prototype.

--- rebol/sieve.r
+++ rebol/sieve.r
@@ -4,58 +4,57 @@ REBOL [
     Exports: [make-sieve]
 ]
 
-; TODO: Use a prototype instead of defining it for each.
+sieve-proto: make object! [
+    limit: 8192
+    composites: none
 
-make-sieve: funct [] [
-    tmp: make object! [
-	limit: 8192
-	composites: none
+    fill: func [/local pos n] [
+	composites: make bitset! (limit + 1)
+	composites/0: true
+	composites/1: true
 
-	fill: func [/local pos n] [
-	    composites: make bitset! (limit + 1)
-	    composites/0: true
-	    composites/1: true
-
-	    pos: 2
-	    while [pos <= limit] [
-		either composites/(pos) [
+	pos: 2
+	while [pos <= limit] [
+	    either composites/(pos) [
+		pos: pos + 1
+	    ] [
+		n: pos + pos
+		while [n <= limit] [
+		    composites/(n): true
+		    n: n + pos
+		]
+		either pos == 2 [
 		    pos: pos + 1
 		] [
-		    n: pos + pos
-		    while [n <= limit] [
-			composites/(n): true
-			n: n + pos
-		    ]
-		    either pos == 2 [
-			pos: pos + 1
-		    ] [
-			pos: pos + 2
-		    ]
+		    pos: pos + 2
 		]
-	    ]
-	]
-
-	prime?: func [n [integer!]] [
-	    if n >= limit [
-		while [limit < n] [
-		    limit: limit * 8
-		]
-		fill
-	    ]
-	    not composites/(n)
-	]
-
-	next-prime: func [n [integer!]] [
-	    either n == 2 [ 3 ] [
-		n: n + 2
-		while [not prime? n] [
-		    n: n + 2
-		]
-		n
 	    ]
 	]
     ]
 
+    prime?: func [n [integer!]] [
+	if n >= limit [
+	    while [limit < n] [
+		limit: limit * 8
+	    ]
+	    fill
+	]
+	not composites/(n)
+    ]
+
+    next-prime: func [n [integer!]] [
+	either n == 2 [ 3 ] [
+	    n: n + 2
+	    while [not prime? n] [
+		n: n + 2
+	    ]
+	    n
+	]
+    ]
+]
+
+make-sieve: funct [] [
+    tmp: make sieve-proto []
     tmp/fill
     tmp
 ]
