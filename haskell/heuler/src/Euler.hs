9023ec52d3e935fd2e13e618f1f0177dc09872e5
David Brown <davidb@davidb.org>
2017-07-22 13:14:17 -0600

    haskell: Move 8

--- haskell/heuler/src/Euler.hs
+++ haskell/heuler/src/Euler.hs
@@ -11,9 +11,10 @@ import Euler.Pr004
 import Euler.Pr005
 import Euler.Pr006
 import Euler.Pr007
+import Euler.Pr008
 import Data.IntMap (IntMap)
 import qualified Data.IntMap as M
 
 problems :: IntMap Problem
 problems = M.fromList $ map (\p -> (prNumber p, p)) [
-   pr001, pr002, pr003, pr004, pr005, pr006, pr007 ]
+   pr001, pr002, pr003, pr004, pr005, pr006, pr007, pr008 ]
