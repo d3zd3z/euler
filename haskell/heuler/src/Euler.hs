dce917a44d92ffdba632f5383ef26f06e72fd48a
David Brown <davidb@davidb.org>
2017-09-10 14:00:58 -0600

    Update new haskell code for pr11

--- haskell/heuler/src/Euler.hs
+++ haskell/heuler/src/Euler.hs
@@ -14,9 +14,11 @@ import Euler.Pr007
 import Euler.Pr008
 import Euler.Pr009
 import Euler.Pr010
+import Euler.Pr011
 import Data.IntMap (IntMap)
 import qualified Data.IntMap as M
 
 problems :: IntMap Problem
 problems = M.fromList $ map (\p -> (prNumber p, p)) [
-   pr001, pr002, pr003, pr004, pr005, pr006, pr007, pr008, pr009, pr010 ]
+   pr001, pr002, pr003, pr004, pr005, pr006, pr007, pr008, pr009, pr010,
+   pr011 ]
