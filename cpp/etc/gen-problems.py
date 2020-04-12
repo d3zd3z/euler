ddedc4550c5cf0b2d7f465034a2ffd8972e0e827
David Brown <davidb@davidb.org>
2020-04-11 22:47:46 -0600

    cpp: Change generator to python3

--- cpp/etc/gen-problems.py
+++ cpp/etc/gen-problems.py
@@ -3,19 +3,19 @@
 import sys
 
 if __name__ == '__main__':
-    print '''// Auto-generated, do not edit.
+    print('''// Auto-generated, do not edit.
 
 #include <vector>
 #include "problems.hpp"
 
 namespace euler {
-'''
+''')
 
     for problem in sys.argv[1:]:
         print("namespace pr{0} {{ void solve(); }}".format(problem))
 
-    print '''const std::vector<Problem> problems {'''
+    print('''const std::vector<Problem> problems {''')
     for problem in sys.argv[1:]:
         print("  {{ {0}, pr{1}::solve }},".format(int(problem), problem))
-    print '''};
-}'''
+    print('''};
+}''')
