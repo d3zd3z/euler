554468c8abfa7a23284422b9eadbb0fb9314c58e
David Brown <davidb@davidb.org>
2020-04-12 11:00:22 -0600

    ocaml: 60: Simpler solution
    Instead of searching the chain each time, build up a list the groups.
    There will be more primality tests here, but there isn't as much
    searching.
    
    The search time is still dominated by generating the enormous prime
    sieve (these get into 8-digit primes), and the fix there will be to use
    something like Miller-Rabin for primality testing.

--- ocaml/pr060.ml
+++ ocaml/pr060.ml
@@ -21,18 +21,6 @@ let goodpair sv a b =
   Sieve.is_prime sv (concatnum a b) &&
     Sieve.is_prime sv (concatnum b a)
 
-(* Given a list of primes (descending order), add the next prime, and
- * produce a list of prime pairs with that new prime.  Returns the new
- * prime list, and a list of the pairs that are valid according to the
- * rules. *)
-let next_pairs sv = function
-  | [] -> (2, [2], [])
-  | (a::_) as aa ->
-      let n = Sieve.next_prime sv a in
-      let pairs = List.filter_map aa ~f:(fun p ->
-        if goodpair sv n p then Some p else None) in
-      (n, n :: aa, pairs)
-
 (* This search builds up mappings of pairs, and looks for chains
  * through that map having at least the given length.  This search
  * works, although it needs quite a bit of memory (>2GB), and seems
@@ -44,72 +32,46 @@ let next_pairs sv = function
 module Searcher = struct
   type t = {
     sieve : Sieve.t;
-    primes : int list;
-    pairto : Int.Set.t Int.Map.t;
+    prime : int;
+    groups : int list list;
   }
 
   type intlist = int list [@@deriving show]
-  type pairlist = (int * int list) list [@@deriving show]
+  type intlistlist = int list list [@@deriving show]
 
   let show t =
-    let pairto = Int.Map.map t.pairto ~f:Int.Set.to_list in
-    let pairto = Int.Map.to_alist pairto in
-    sprintf "{ primes = %s; pairto = %s }"
-      (show_intlist t.primes)
-      (show_pairlist pairto)
+    sprintf "{ prime = %s; groups = %s }"
+      (Int.to_string t.prime)
+      (show_intlistlist t.groups)
 
   let init sieve = {
     sieve;
-    primes = [];
-    pairto = Int.Map.empty;
+    prime = 2;
+    groups = [[]; [2]];
   }
 
   let print_info t =
-    let primes = List.length t.primes in
-    let pairs = Int.Map.data t.pairto in
-    let pairs = List.map pairs ~f:Int.Set.length in
-    let pairs = List.fold pairs ~init:0 ~f:(+) in
-    printf "Searcher %d primes, %d pairs\n%!" primes pairs
+    let groups = List.map t.groups ~f:List.length in
+    let groups = List.fold groups ~init:0 ~f:(+) in
+    printf "Searcher %d prime, %d group members\n%!" t.prime groups
 
+  (* Add a new prime.  Will create new groups for any where all
+   * numbers match *)
   let add_prime t =
-    let new_prime, primes, pairs = next_pairs t.sieve t.primes in
-    let add_pair m a b =
-      Int.Map.change m a ~f:(function
-        | None -> Some (Int.Set.singleton b)
-        | Some s -> Some (Int.Set.add s b)) in
-    let pairto = List.fold ~init:t.pairto pairs ~f:(fun m a ->
-      add_pair m a new_prime) in
-    { t with primes; pairto }
-
-  (* Search the current data, looking for a chain of length 'len'. *)
-  exception Scan_result of int list
-  let scan t len =
-    let rec subscan seen to_check =
-      if Int.Set.length seen = len then
-        raise (Scan_result (Int.Set.to_list seen))
+    let prime = Sieve.next_prime t.sieve t.prime in
+    let groups = List.concat_map t.groups ~f:(fun gr ->
+      if List.for_all gr ~f:(fun p -> goodpair t.sieve prime p) then
+        [(prime :: gr); gr]
       else
-        let rec loop = function
-          | [] -> ()
-          | (p2 :: rest) ->
-              (* Ensure that p2 references all of the others. *)
-              let p2_set =
-                match Int.Map.find t.pairto p2 with
-                  | None -> Int.Set.empty
-                  | Some x -> x in
-              if Int.Set.for_all seen ~f:(fun x -> Int.Set.mem p2_set x) then
-                subscan (Int.Set.add seen p2) rest;
-              loop rest
-        in loop to_check
-    in
-    match t.primes with
-      | [] -> failwith "Cannot scan without primes"
-      | (n :: rest) ->
-          let start_set = Int.Set.singleton n in
-          subscan start_set rest
+        [gr]) in
+    { t with prime; groups }
 
   let scan t len =
-    try (scan t len; None) with
-      | Scan_result x -> Some x
+    let groups = List.filter t.groups ~f:(fun g -> List.length g = len) in
+    match groups with
+      | [g] -> Some g
+      | [] -> None
+      | _ -> failwith "Found multiple groups, need to select smallest"
 end
 
 let solve () =
